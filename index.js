4e3e7acddbd424584425a725f9296079409a9f92
Justin <jmp86@yahoo.com>
2021-04-28 15:40:38 -1000

    commit tree fac0c390b38274e0db64202edb1e086d18d26b57

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-function addingEventListener() {
-    function clickAlert() {
-        alert('I was clicked!');
-      }
-        input.addEventListener('click', clickAlert);
-}
 
 const input = document.getElementById('input');
 
-
 const body = document.getElementsByTagName('body')[0];
-
 
 const secondChild = body.children[4];
 
@@ -17,10 +9,17 @@ const div1 = secondChild.getElementsByTagName('div')[0];
 
 const div2 = div1.getElementsByTagName('div')[0];
 
-
 const div3 = div2.getElementsByTagName('div')[0];
-
 
 const div4 = div3.getElementsByTagName('div')[0];
 
-div4.addEventListener('click', clickAlert);
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+function addingEventListener() {
+      input.addEventListener('click', clickAlert);
+      div2.addEventListener('click', clickAlert);
+  }
+
+  addingEventListener();
